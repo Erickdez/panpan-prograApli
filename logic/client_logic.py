847257d60aa1f26060ebea7db01758930dab735e
Erick <yerickhf16@outlook.com>
2021-06-09 14:36:11 -0600

    Client Logic

---
+++ logic/client_logic.py
@@ -0,0 +1,28 @@
+from core.pyba_logic import PybaLogic
+
+
+class ClientLogic(PybaLogic):
+    def __init__(self):
+        super().__init__()
+
+    def insertClient(self, clientName, clientEmail, clientCel, password, salt):
+        database = self.createDatabaseObj()
+        sql = (
+            "INSERT INTO `panpanbd`.`clients` "
+            + "(`id_client`,`email`,`cel`,`name`,`password`,`salt`) "
+            + f"VALUES(0,'{clientEmail}','{clientCel}','{clientName}','{password}','{salt}');"
+        )
+        rows = database.executeNonQueryRows(sql)
+        return rows
+
+    def getClientByEmail(self, clientEmail):
+        database = self.createDatabaseObj()
+        sql = (
+            "SELECT email, password, salt "
+            + f"FROM panpanbd.clients where email like '{clientEmail}';"
+        )
+        result = database.executeQuery(sql)
+        if len(result) > 0:
+            return result[0]
+        else:
+            return []
